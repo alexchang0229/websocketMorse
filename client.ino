44e8614ae8f419e29cf55553661efb278cf6ca22
Alex <alex_chang@live.ca>
2023-04-01 10:26:28 -0400

    channels working

--- client.ino
+++ client.ino
@@ -1,19 +1,26 @@
 #include <ArduinoWebsockets.h>
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>
-#define inMessageBuf_SIZE 30
+
 #define keyPin 4
+#define LedPin 16
+#define channelPin 0
+
 int keyState = 0;
 int lastkeyState = 0;
 const char *initials = "AC";
-unsigned long inMessageTime, playbackStart = 0, messageStart = 0, lastKeyTime = 0, lastInMessageTime = 0;
-unsigned long inMessageBuf[inMessageBuf_SIZE];
-int bufInd = 0, playbackInd = 0;
-bool isMessaging = false, inState;
-char buf[30], messageOut[50];
+String channels[10] = {"ABC"};
+bool channelButtonState, prevChannelButtonState;
+int channelNumber = 0;
+unsigned long messageStart = 0, lastKeyTime = 0, lastHeartBeatTime = 0;
+bool isMessaging = false;
+char messageOut[100];
 const char *websockets_server_host = "192.168.0.36"; // Enter server adress //192.168.2.27
 const uint16_t websockets_server_port = 8001;        // Enter server port
-String messageIn[50], oprInitials;
+String messageIn[50];
+StaticJsonDocument<200> outJSON, inJSON;
+
 using namespace websockets;
 
 WebsocketsClient client;
@@ -22,7 +29,6 @@ void setup()
   Serial.begin(115200);
   // Connect to wifi
   WiFiManager wm;
-  //wm.resetSettings();
   bool res;
   res = wm.autoConnect("AutoConnectAP");
   if (!res) {
@@ -32,13 +38,15 @@ void setup()
 
   // try to connect to Websockets server
   bool connected = client.connect(websockets_server_host, websockets_server_port, "/ws");
-  if (connected)
-  {
+  if (connected) {
     Serial.println("Connected!");
-    client.send("Hello Server");
+    outJSON["initials"] = initials;
+    outJSON["channel"] = channels[channelNumber];
+    outJSON["action"] = "subscribe";
+    serializeJson(outJSON, messageOut);
+    client.send(messageOut);
   }
-  else
-  {
+  else {
     Serial.println("Not Connected!");
   }
 
@@ -46,19 +54,18 @@ void setup()
   client.onMessage([&](WebsocketsMessage message)
   {
     Serial.println(message.data());
-    if (message.data()[2] == 'T' || message.data()[2] == 'F') {
-      oprInitials = message.data().substring(0, 2);
-      //if (!oprInitials.equals(initials)) {
-      if (message.data()[2] == 'T') {
-        inState = true;
+    DeserializationError err = deserializeJson(inJSON, message.data());
+    if (!err) {
+      //if (!inJSON["initals"].equals(initials)) {
+      bool inState = inJSON["state"];
+      const char* inInitials = inJSON["initials"];
+      if (inState == 1) {
+        tone(12, 523);
       } else {
-        inState = false;
+        noTone(12);
       }
-      inMessageTime = message.data().substring(3).toInt();
-      inMessageBuf[bufInd] = inMessageTime;
-      bufInd = (bufInd + 1) % inMessageBuf_SIZE;
-      //}
     }
+    //}
   });
 }
 
@@ -67,75 +74,64 @@ void loop() {
   if (client.available()) {
     client.poll();
   }
-  if (inMessageBuf[playbackInd] >= lastInMessageTime) {
-    Serial.print("STATE:");
-    Serial.print(inState);
-    Serial.print("|millis:");
-    Serial.print(millis());
-    Serial.print("|in + start:");
-    Serial.println(inMessageBuf[playbackInd] + playbackStart);
-    if (inMessageBuf[playbackInd] == 0 && inState == 1) {
-      Serial.println("first beep");
-      playbackStart = millis();
-      tone(12, 523);
-      lastInMessageTime = inMessageBuf[playbackInd];
-      playbackInd = (playbackInd + 1) % inMessageBuf_SIZE;
-    }
-    if (inMessageTime != 0) {
-      if (millis() >= inMessageBuf[playbackInd] + playbackStart) {
-        if (inState == 1) {
-          Serial.print("T:");
-          Serial.println(inMessageBuf[playbackInd]);
-          tone(12, 523);
-        } else {
-          Serial.print("F:");
-          Serial.println(inMessageBuf[playbackInd]);
-          noTone(12);
-        }
-         lastInMessageTime = inMessageBuf[playbackInd];
-         playbackInd = (playbackInd + 1) % inMessageBuf_SIZE;
-      }
-    }
-   
-  }
 
-
+  if (millis() - lastHeartBeatTime > 3 * 1000) {
+    client.send("HeartBeat");
+    lastHeartBeatTime = millis();
+  };
 
   keyState = digitalRead(keyPin);
   //----------- PROCESS TELEGRAPH KEY PRESSES -----------//
   if (keyState == 1 && lastkeyState != keyState) {
     //tone(12, 523);
+    outJSON["initials"] = initials;
+    outJSON["state"] = true;
+    outJSON["action"] = "key";
     if (isMessaging == false) {
       messageStart = millis();
-      strcpy(messageOut, initials);
-      strcat(messageOut, "T0");
-      client.send(messageOut);
+      outJSON["time"] = 0;
       isMessaging = true;
     } else {
-      ultoa(millis() - messageStart, buf, 10);
-      strcpy(messageOut, initials);
-      strcat(messageOut, "T");
-      strcat(messageOut, buf);
-      //Serial.println(messageOut);
+      outJSON["time"] = millis() - messageStart;
       client.send(messageOut);
     }
+    serializeJson(outJSON, messageOut);
+    client.send(messageOut);
     lastKeyTime = millis();
   }
   if (keyState == 0 && lastkeyState != keyState) {
     //noTone(12);
-    ultoa(millis() - messageStart, buf, 10);
-    strcpy(messageOut, initials);
-    strcat(messageOut, "F");
-    strcat(messageOut, buf);
-    //Serial.println(messageOut);
+    outJSON["initials"] = initials;
+    outJSON["state"] = false;
+    outJSON["time"] = millis() - messageStart;
+    outJSON["action"] = "key";
+    serializeJson(outJSON, messageOut);
     client.send(messageOut);
   }
   lastkeyState = keyState;
 
   if (millis() - lastKeyTime > 5 * 1000 && isMessaging == true) {
-    //Serial.println(millis() - lastKeyTime);
     isMessaging = false;
   }
 
-  delay(3);
+  channelButtonState = digitalRead(channelPin);
+  if(channelButtonState == 0 && channelButtonState!=prevChannelButtonState){
+    changeChannels();
+  }
+  prevChannelButtonState = channelButtonState;
+
+  delay(10);
+}
+
+void changeChannels() {
+    channelNumber = (channelNumber + 1) % 2  ;
+    for(int i=0;i<channelNumber +1;i++){
+      tone(12,523);delay(100);noTone(12);
+    }
+    outJSON["initials"] = initials;
+    outJSON["channel"] = channels[channelNumber];
+    outJSON["action"] = "subscribe";
+    serializeJson(outJSON, messageOut);
+    client.send(messageOut);
+    Serial.print(channelNumber);
 }
